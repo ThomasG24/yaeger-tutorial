b3a38412f5a07d05d00a7d551aaa031600ddf237
ThomasG24 <102722308+ThomasG24@users.noreply.github.com>
2025-03-25 14:35:52 +0100

    verandering

--- src/main/java/com/github/hanyaeger/tutorial/HealthText.java
+++ src/main/java/com/github/hanyaeger/tutorial/HealthText.java
@@ -13,7 +13,6 @@ public class HealthText extends TextEntity {
         setFont(Font.font("Roboto", FontWeight.NORMAL, 30));
         setFill(Color.DARKBLUE);
     }
-
     public void setHealthText(int health) {
         setText("Health: " + health);
     }
